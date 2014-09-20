ab4de5a5506c7eb6e3942a2d17d5b3cf056a317b
Bjørn Rostad <hi@bjornrostad.no>
2014-09-20 11:28:40 -0700

    started working on auth

--- Carousel Prototype/SignInViewController.swift
+++ Carousel Prototype/SignInViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class SignInViewController: UIViewController, UIAlertViewDelegate {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func pressBackButton(sender: UIButton) {
         navigationController!.popViewControllerAnimated(true)
     }
     
     @IBAction func pressSignInButton(sender: UIButton) {
+        if emailTextField.text == "hi@bjornrostad.no" && passwordTextField.text == "login" {
+            
+        } else if emailTextField.text == "" || passwordTextField.text == "" {
+            var alertView = UIAlertView(title: "Email Required", message: "Please enter your email address", delegate: self, cancelButtonTitle: "OK")
+            alertView.show()
+            
+            // after press ok, set cursor to the email or password field
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var alertView = UIAlertView(title: "Email Required", message: "Please enter your email address", delegate: self, cancelButtonTitle: "OK")
-        alertView.show()
+        
         
     }
 
