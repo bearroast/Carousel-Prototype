ad2d6a4531ac51fe810b1e95cdc7a83450631139
Bjørn Rostad <hi@bjornrostad.no>
2014-09-20 15:22:57 -0700

    more stuff for tutorial

--- Carousel Prototype/TutorialViewController.swift
+++ Carousel Prototype/TutorialViewController.swift
@@ -11,55 +11,55 @@ import UIKit
 class TutorialViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var backupButtonImageView: UIImageView!
+    @IBAction func onBackUpButton(sender: UIButton) {
+        
+    }
 
     
     func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
         // Get the current page based on the scroll offset
-        var page : Int = Int(round(scrollView.contentOffset.x / 320))
+        var page:Int = Int(round(scrollView.contentOffset.x / 320))
         
         // Set the current page, so the dots will update
         pageControl.currentPage = page
+        
+        
+        // Set the backup button to only be visible on the last page
+        if page == 3 {
+            
+            backupButtonImageView.alpha = 1
+        }
+        else {
+            backupButtonImageView.alpha = 0
+        }
     }
 
-    func scrollViewDidScroll(scrollView: UIScrollView!) {
-        // This method is called as the user scrolls
-    }
-    
-    func scrollViewWillBeginDragging(scrollView: UIScrollView!) {
-        
-    }
-    
-    func scrollViewDidEndDragging(scrollView: UIScrollView!,
-        willDecelerate decelerate: Bool) {
-            // This method is called right as the user lifts their finger
-    }
-    
-//    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
-//        // This method is called when the scrollview finally stops scrolling.
+//    func scrollViewDidScroll(scrollView: UIScrollView!) {
+//        // This method is called as the user scrolls
 //    }
+//    
+//    func scrollViewWillBeginDragging(scrollView: UIScrollView!) {
+//        
+//    }
+//    
+//    func scrollViewDidEndDragging(scrollView: UIScrollView!,
+//        willDecelerate decelerate: Bool) {
+//            // This method is called right as the user lifts their finger
+//    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView.contentSize = CGSize(width: 320, height: 568)
-        
+        scrollView.contentSize = CGSize(width: 1280, height: 568)
         scrollView.delegate = self
+        
+
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
